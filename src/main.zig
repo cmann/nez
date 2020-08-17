4d8f47fbb78709a8c87de0f7837cdb382bdef2ba
Cameron Mann <cameron.mann@cybera.ca>
2020-08-17 17:58:42 -0600

    Start adding opcodes and address modes

--- src/main.zig
+++ src/main.zig
@@ -44,6 +44,71 @@ const CPU = struct {
     }
 };
 
+// Address Modes:
+//
+// A        Accumulator            OPC A         operand is AC (implied single byte instruction)
+// abs      absolute               OPC $LLHH     operand is address $HHLL *
+// abs,X    absolute, X-indexed    OPC $LLHH,X   operand is address; effective address is address incremented by X with carry **
+// abs,Y    absolute, Y-indexed    OPC $LLHH,Y   operand is address; effective address is address incremented by Y with carry **
+// #        immediate              OPC #$BB      operand is byte BB
+// impl     implied                OPC           operand implied
+// ind      indirect               OPC ($LLHH)   operand is address; effective address is contents of word at address: C.w($HHLL)
+// X,ind    X-indexed, indirect    OPC ($LL,X)   operand is zeropage address; effective address is word in (LL + X, LL + X + 1), inc. without carry: C.w($00LL + X)
+// ind,Y    indirect, Y-indexed    OPC ($LL),Y   operand is zeropage address; effective address is word in (LL, LL + 1) incremented by Y with carry: C.w($00LL) + Y
+// rel      relative               OPC $BB       branch target is PC + signed offset BB ***
+// zpg      zeropage               OPC $LL       operand is zeropage address (hi-byte is zero, address = $00LL)
+// zpg,X    zeropage, X-indexed    OPC $LL,X     operand is zeropage address; effective address is address incremented by X without carry **
+// zpg,Y    zeropage, Y-indexed    OPC $LL,Y     operand is zeropage address; effective address is address incremented by Y without carry **
+//
+// *   16-bit address words are little endian, lo(w)-byte first, followed by the high-byte.
+// (An assembler will use a human readable, big-endian notation as in $HHLL.)
+//
+// **  The available 16-bit address space is conceived as consisting of pages of 256 bytes each, with
+// address hi-bytes representing the page index. An increment with carry may affect the hi-byte
+// and may thus result in a crossing of page boundaries, adding an extra cycle to the execution.
+// Increments without carry do not affect the hi-byte of an address and no page transitions do occur.
+// Generally, increments of 16-bit addresses include a carry, increments of zeropage addresses don't.
+// Notably this is not related in any way to the state of the carry bit of the accumulator.
+//
+// *** Branch offsets are signed 8-bit values, -128 ... +127, negative offsets in two's complement.
+// Page transitions may occur and add an extra cycle to the execution.
+const instructions = [_]u8{
+    //  00           01        02       03       04           05           06        07        08          09          0A        0B       0C           0D           0E         0F
+    "BRK impl", "ORA X,ind", "---",   "---", "---",       "ORA zpg",   "ASL zpg",   "---", "PHP impl", "ORA #",     "ASL A",    "---", "---",       "ORA abs",   "ASL abs",   "---", // 00
+    "BPL rel",  "ORA ind,Y", "---",   "---", "---",       "ORA zpg,X", "ASL zpg,X", "---", "CLC impl", "ORA abs,Y", "---",      "---", "---",       "ORA abs,X", "ASL abs,X", "---", // 10
+    "JSR abs",  "AND X,ind", "---",   "---", "BIT zpg",   "AND zpg",   "ROL zpg",   "---", "PLP impl", "AND #",     "ROL A",    "---", "BIT abs",   "AND abs",   "ROL abs",   "---", // 20
+    "BMI rel",  "AND ind,Y", "---",   "---", "---",       "AND zpg,X", "ROL zpg,X", "---", "SEC impl", "AND abs,Y", "---",      "---", "---",       "AND abs,X", "ROL abs,X", "---", // 30
+    "RTI impl", "EOR X,ind", "---",   "---", "---",       "EOR zpg",   "LSR zpg",   "---", "PHA impl", "EOR #",     "LSR A",    "---", "JMP abs",   "EOR abs",   "LSR abs",   "---", // 40
+    "BVC rel",  "EOR ind,Y", "---",   "---", "---",       "EOR zpg,X", "LSR zpg,X", "---", "CLI impl", "EOR abs,Y", "---",      "---", "---",       "EOR abs,X", "LSR abs,X", "---", // 50
+    "RTS impl", "ADC X,ind", "---",   "---", "---",       "ADC zpg",   "ROR zpg",   "---", "PLA impl", "ADC #",     "ROR A",    "---", "JMP ind",   "ADC abs",   "ROR abs",   "---", // 60
+    "BVS rel",  "ADC ind,Y", "---",   "---", "---",       "ADC zpg,X", "ROR zpg,X", "---", "SEI impl", "ADC abs,Y", "---",      "---", "---",       "ADC abs,X", "ROR abs,X", "---", // 70
+    "---",      "STA X,ind", "---",   "---", "STY zpg",   "STA zpg",   "STX zpg",   "---", "DEY impl", "---",       "TXA impl", "---", "STY abs",   "STA abs",   "STX abs",   "---", // 80
+    "BCC rel",  "STA ind,Y", "---",   "---", "STY zpg,X", "STA zpg,X", "STX zpg,Y", "---", "TYA impl", "STA abs,Y", "TXS impl", "---", "---",       "STA abs,X", "---",       "---", // 90
+    "LDY #",    "LDA X,ind", "LDX #", "---", "LDY zpg",   "LDA zpg",   "LDX zpg",   "---", "TAY impl", "LDA #",     "TAX impl", "---", "LDY abs",   "LDA abs",   "LDX abs",   "---", // A0
+    "BCS rel",  "LDA ind,Y", "---",   "---", "LDY zpg,X", "LDA zpg,X", "LDX zpg,Y", "---", "CLV impl", "LDA abs,Y", "TSX impl", "---", "LDY abs,X", "LDA abs,X", "LDX abs,Y", "---", // B0
+    "CPY #",    "CMP X,ind", "---",   "---", "CPY zpg",   "CMP zpg",   "DEC zpg",   "---", "INY impl", "CMP #",     "DEX impl", "---", "CPY abs",   "CMP abs",   "DEC abs",   "---", // C0
+    "BNE rel",  "CMP ind,Y", "---",   "---", "---",       "CMP zpg,X", "DEC zpg,X", "---", "CLD impl", "CMP abs,Y", "---",      "---", "---",       "CMP abs,X", "DEC abs,X", "---", // D0
+    "CPX #",    "SBC X,ind", "---",   "---", "CPX zpg",   "SBC zpg",   "INC zpg",   "---", "INX impl", "SBC #",     "NOP impl", "---", "CPX abs",   "SBC abs",   "INC abs",   "---", // E0
+    "BEQ rel",  "SBC ind,Y", "---",   "---", "---",       "SBC zpg,X", "INC zpg,X", "---", "SED impl", "SBC abs,Y", "---",      "---", "---",       "SBC abs,X", "INC abs,X", "---", // F0
+};
+
+const addressModes = enum {
+    // Non-indexed, non-memory
+    Accumulator, // OPC A
+    Immediate, // OPC #$BB
+    Implied, // OPC
+    // Non-indexed, memory
+    Relative,
+    Absolute,
+    ZeroPage,
+    Indirect,
+    // Indexed, memory
+    AbsoluteIndexed,
+    ZeroPageIndexed,
+    IndexedIndirect,
+    IndirectIndexed,
+};
+
 pub fn main() !void {
     a = std.heap.c_allocator;
 
@@ -57,6 +122,14 @@ pub fn main() !void {
 
     const bin = try (fs.cwd().readFileAlloc(a, bin_path, 1024 * 100));
     defer a.free(bin);
+
+    var i: usize = 0x0400;
+    while (i < bin.len - 1) : (i += 2) {
+        const hi = bin[i];
+        const lo = bin[i + 1];
+
+        std.debug.warn("{x:2} {x:2}\n", .{ hi, lo });
+    }
 
     var cpu = CPU.powerOn();
 }

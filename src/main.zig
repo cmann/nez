cda66557789ec72d8d7fa7b4f3b25fdf8f12fd3d
Cameron Mann <cameron.mann@cybera.ca>
2020-08-18 14:43:46 -0600

    More work on instruction decoding

--- src/main.zig
+++ src/main.zig
@@ -4,15 +4,18 @@ const process = std.process;
 
 var a: *std.mem.Allocator = undefined;
 
-const Flags = struct {
-    carry: bool,
-    zero: bool,
-    interruptDisable: bool,
-    decimalMode: bool,
-    brk: bool,
-    unused: bool,
-    overflow: bool,
-    negative: bool,
+const StatusRegister = packed union {
+    p: u8,
+    flags: packed struct {
+        c: bool,
+        z: bool,
+        i: bool,
+        d: bool,
+        b: bool,
+        u: bool,
+        v: bool,
+        n: bool,
+    },
 };
 
 const Registers = struct {
@@ -43,86 +46,265 @@ const Context = struct {
     }
 };
 
-// Address Modes:
-//
-// A        Accumulator            OPC A         operand is AC (implied single byte instruction)
-// abs      absolute               OPC $LLHH     operand is address $HHLL *
-// abs,X    absolute, X-indexed    OPC $LLHH,X   operand is address; effective address is address incremented by X with carry **
-// abs,Y    absolute, Y-indexed    OPC $LLHH,Y   operand is address; effective address is address incremented by Y with carry **
-// #        immediate              OPC #$BB      operand is byte BB
-// impl     implied                OPC           operand implied
-// ind      indirect               OPC ($LLHH)   operand is address; effective address is contents of word at address: C.w($HHLL)
-// X,ind    X-indexed, indirect    OPC ($LL,X)   operand is zeropage address; effective address is word in (LL + X, LL + X + 1), inc. without carry: C.w($00LL + X)
-// ind,Y    indirect, Y-indexed    OPC ($LL),Y   operand is zeropage address; effective address is word in (LL, LL + 1) incremented by Y with carry: C.w($00LL) + Y
-// rel      relative               OPC $BB       branch target is PC + signed offset BB ***
-// zpg      zeropage               OPC $LL       operand is zeropage address (hi-byte is zero, address = $00LL)
-// zpg,X    zeropage, X-indexed    OPC $LL,X     operand is zeropage address; effective address is address incremented by X without carry **
-// zpg,Y    zeropage, Y-indexed    OPC $LL,Y     operand is zeropage address; effective address is address incremented by Y without carry **
-//
-// *   16-bit address words are little endian, lo(w)-byte first, followed by the high-byte.
-// (An assembler will use a human readable, big-endian notation as in $HHLL.)
-//
-// **  The available 16-bit address space is conceived as consisting of pages of 256 bytes each, with
-// address hi-bytes representing the page index. An increment with carry may affect the hi-byte
-// and may thus result in a crossing of page boundaries, adding an extra cycle to the execution.
-// Increments without carry do not affect the hi-byte of an address and no page transitions do occur.
-// Generally, increments of 16-bit addresses include a carry, increments of zeropage addresses don't.
-// Notably this is not related in any way to the state of the carry bit of the accumulator.
-//
-// *** Branch offsets are signed 8-bit values, -128 ... +127, negative offsets in two's complement.
-// Page transitions may occur and add an extra cycle to the execution.
-const instructions = [_]u8{
-    //  00           01        02       03       04           05           06        07        08          09          0A        0B       0C           0D           0E         0F
-    "BRK impl", "ORA X,ind", "---",   "---", "---",       "ORA zpg",   "ASL zpg",   "---", "PHP impl", "ORA #",     "ASL A",    "---", "---",       "ORA abs",   "ASL abs",   "---", // 00
-    "BPL rel",  "ORA ind,Y", "---",   "---", "---",       "ORA zpg,X", "ASL zpg,X", "---", "CLC impl", "ORA abs,Y", "---",      "---", "---",       "ORA abs,X", "ASL abs,X", "---", // 10
-    "JSR abs",  "AND X,ind", "---",   "---", "BIT zpg",   "AND zpg",   "ROL zpg",   "---", "PLP impl", "AND #",     "ROL A",    "---", "BIT abs",   "AND abs",   "ROL abs",   "---", // 20
-    "BMI rel",  "AND ind,Y", "---",   "---", "---",       "AND zpg,X", "ROL zpg,X", "---", "SEC impl", "AND abs,Y", "---",      "---", "---",       "AND abs,X", "ROL abs,X", "---", // 30
-    "RTI impl", "EOR X,ind", "---",   "---", "---",       "EOR zpg",   "LSR zpg",   "---", "PHA impl", "EOR #",     "LSR A",    "---", "JMP abs",   "EOR abs",   "LSR abs",   "---", // 40
-    "BVC rel",  "EOR ind,Y", "---",   "---", "---",       "EOR zpg,X", "LSR zpg,X", "---", "CLI impl", "EOR abs,Y", "---",      "---", "---",       "EOR abs,X", "LSR abs,X", "---", // 50
-    "RTS impl", "ADC X,ind", "---",   "---", "---",       "ADC zpg",   "ROR zpg",   "---", "PLA impl", "ADC #",     "ROR A",    "---", "JMP ind",   "ADC abs",   "ROR abs",   "---", // 60
-    "BVS rel",  "ADC ind,Y", "---",   "---", "---",       "ADC zpg,X", "ROR zpg,X", "---", "SEI impl", "ADC abs,Y", "---",      "---", "---",       "ADC abs,X", "ROR abs,X", "---", // 70
-    "---",      "STA X,ind", "---",   "---", "STY zpg",   "STA zpg",   "STX zpg",   "---", "DEY impl", "---",       "TXA impl", "---", "STY abs",   "STA abs",   "STX abs",   "---", // 80
-    "BCC rel",  "STA ind,Y", "---",   "---", "STY zpg,X", "STA zpg,X", "STX zpg,Y", "---", "TYA impl", "STA abs,Y", "TXS impl", "---", "---",       "STA abs,X", "---",       "---", // 90
-    "LDY #",    "LDA X,ind", "LDX #", "---", "LDY zpg",   "LDA zpg",   "LDX zpg",   "---", "TAY impl", "LDA #",     "TAX impl", "---", "LDY abs",   "LDA abs",   "LDX abs",   "---", // A0
-    "BCS rel",  "LDA ind,Y", "---",   "---", "LDY zpg,X", "LDA zpg,X", "LDX zpg,Y", "---", "CLV impl", "LDA abs,Y", "TSX impl", "---", "LDY abs,X", "LDA abs,X", "LDX abs,Y", "---", // B0
-    "CPY #",    "CMP X,ind", "---",   "---", "CPY zpg",   "CMP zpg",   "DEC zpg",   "---", "INY impl", "CMP #",     "DEX impl", "---", "CPY abs",   "CMP abs",   "DEC abs",   "---", // C0
-    "BNE rel",  "CMP ind,Y", "---",   "---", "---",       "CMP zpg,X", "DEC zpg,X", "---", "CLD impl", "CMP abs,Y", "---",      "---", "---",       "CMP abs,X", "DEC abs,X", "---", // D0
-    "CPX #",    "SBC X,ind", "---",   "---", "CPX zpg",   "SBC zpg",   "INC zpg",   "---", "INX impl", "SBC #",     "NOP impl", "---", "CPX abs",   "SBC abs",   "INC abs",   "---", // E0
-    "BEQ rel",  "SBC ind,Y", "---",   "---", "---",       "SBC zpg,X", "INC zpg,X", "---", "SED impl", "SBC abs,Y", "---",      "---", "---",       "SBC abs,X", "INC abs,X", "---", // F0
+const Instruction = enum {
+    ADC,
+    AND,
+    ASL,
+    BCC,
+    BCS,
+    BEQ,
+    BIT,
+    BMI,
+    BNE,
+    BPL,
+    BRK,
+    BVC,
+    BVS,
+    CLC,
+    CLD,
+    CLI,
+    CLV,
+    CMP,
+    CPX,
+    CPY,
+    DEC,
+    DEX,
+    DEY,
+    EOR,
+    INC,
+    INX,
+    INY,
+    JMP,
+    JSR,
+    LDA,
+    LDX,
+    LDY,
+    LSR,
+    NOP,
+    ORA,
+    PHA,
+    PHP,
+    PLA,
+    PLP,
+    ROL,
+    ROR,
+    RTI,
+    RTS,
+    SBC,
+    SEC,
+    SED,
+    SEI,
+    STA,
+    STX,
+    STY,
+    TAX,
+    TAY,
+    TSX,
+    TXA,
+    TXS,
+    TYA,
 };
 
-const AddressMode = enum {
+const AddressMode = union(enum) {
     // Non-indexed, non-memory
-    Accumulator,
-    Immediate,
-    Implied,
+    Accumulator: void,
+    Immediate: u8,
+    Implied: void,
     // Non-indexed, memory
-    Relative,
-    Absolute,
-    ZeroPage,
+    Relative: u8,
+    Absolute: u16,
+    ZeroPage: u8,
     Indirect,
     // Indexed, memory
-    AbsoluteX,
-    AbsoluteY,
-    ZeroPageX,
-    ZeroPageY,
-    IndexedIndirect,
-    IndirectIndexed,
+    AbsoluteX: u16,
+    AbsoluteY: u16,
+    ZeroPageX: u8,
+    ZeroPageY: u8,
+    IndexedIndirect: u8,
+    IndirectIndexed: u8,
 
-    pub fn load(mode: AddressMode, context: Context) ![]u8 {
-        const val = switch (mode) {
-            Accumulator => context.registers.a,
-            Immediate => context.memory[context.registers.pc + 1],
-            Relative => context.registers.pc + @bitCast(i8, context.memory[context.registers.pc + 1]),
-            Absolute => context.memory[context.memory[context.registers.pc + 1] + context.memory[context.registers.pc + 2]],
-            ZeroPage => context.memory[context.memory[context.registers.pc + 1]],
+    pub fn load(self: AddressMode, context: Context) ![]u8 {
+        const val = switch (self) {
+            .Accumulator => context.registers.a,
+            .Immediate => context.memory[context.registers.pc + 1],
+            .Relative => context.registers.pc + @bitCast(i8, context.memory[context.registers.pc + 1]),
+            .Absolute => context.memory[context.memory[context.registers.pc + 1] + context.memory[context.registers.pc + 2]],
+            .ZeroPage => context.memory[context.memory[context.registers.pc + 1]],
 
-            Implied => 0xff,
+            .Implied => 0xff,
         };
     }
 };
 
-const modes = [_]addressModes{};
+const DecodeError = error{InvalidOpcode};
+
+pub fn decode(opcode: u8, lo: u8, hi: u8) !Instruction {
+    return switch (opcode) {
+        0x00 => .BRK,
+        0x01 => .ORA,
+        0x05 => .ORA,
+        0x06 => .ASL,
+        0x08 => .PHP,
+        0x09 => .ORA,
+        0x0A => .ASL,
+        0x0D => .ORA,
+        0x0E => .ASL,
+        0x10 => .BPL,
+        0x11 => .ORA,
+        0x15 => .ORA,
+        0x16 => .ASL,
+        0x18 => .CLC,
+        0x19 => .ORA,
+        0x1D => .ORA,
+        0x1E => .ASL,
+        0x20 => .JSR,
+        0x21 => .AND,
+        0x24 => .BIT,
+        0x25 => .AND,
+        0x26 => .ROL,
+        0x28 => .PLP,
+        0x29 => .AND,
+        0x2A => .ROL,
+        0x2C => .BIT,
+        0x2D => .AND,
+        0x2E => .ROL,
+        0x30 => .BMI,
+        0x31 => .AND,
+        0x35 => .AND,
+        0x36 => .ROL,
+        0x38 => .SEC,
+        0x39 => .AND,
+        0x3D => .AND,
+        0x3E => .ROL,
+        0x40 => .RTI,
+        0x41 => .EOR,
+        0x45 => .EOR,
+        0x46 => .LSR,
+        0x48 => .PHA,
+        0x49 => .EOR,
+        0x4A => .LSR,
+        0x4C => .JMP,
+        0x4D => .EOR,
+        0x4E => .LSR,
+        0x50 => .BVC,
+        0x51 => .EOR,
+        0x55 => .EOR,
+        0x56 => .LSR,
+        0x58 => .CLI,
+        0x59 => .EOR,
+        0x5D => .EOR,
+        0x5E => .LSR,
+        0x60 => .RTS,
+        0x61 => .ADC,
+        0x65 => .ADC,
+        0x66 => .ROR,
+        0x68 => .PLA,
+        0x69 => .ADC,
+        0x6A => .ROR,
+        0x6C => .JMP,
+        0x6D => .ADC,
+        0x6E => .ROR,
+        0x70 => .BVS,
+        0x71 => .ADC,
+        0x75 => .ADC,
+        0x76 => .ROR,
+        0x78 => .SEI,
+        0x79 => .ADC,
+        0x7D => .ADC,
+        0x7E => .ROR,
+        0x81 => .STA,
+        // 0x83 => .SAX,
+        0x84 => .STY,
+        0x85 => .STA,
+        0x86 => .STX,
+        // 0x87 => .SAX,
+        0x88 => .DEY,
+        0x8A => .TXA,
+        0x8C => .STY,
+        0x8D => .STA,
+        0x8E => .STX,
+        // 0x8F => .SAX,
+        0x90 => .BCC,
+        0x91 => .STA,
+        0x94 => .STY,
+        0x95 => .STA,
+        0x96 => .STX,
+        // 0x97 => .SAX,
+        0x98 => .TYA,
+        0x99 => .STA,
+        0x9A => .TXS,
+        0x9D => .STA,
+        0xA0 => .LDY,
+        0xA1 => .LDA,
+        0xA2 => .LDX,
+        // 0xA3 => .LAX,
+        0xA4 => .LDY,
+        0xA5 => .LDA,
+        0xA6 => .LDX,
+        // 0xA7 => .LAX,
+        0xA8 => .TAY,
+        0xA9 => .LDA,
+        0xAA => .TAX,
+        0xAC => .LDY,
+        0xAD => .LDA,
+        0xAE => .LDX,
+        // 0xAF => .LAX,
+        0xB0 => .BCS,
+        0xB1 => .LDA,
+        // 0xB3 => .LAX,
+        0xB4 => .LDY,
+        0xB5 => .LDA,
+        0xB6 => .LDX,
+        // 0xB7 => .LAX,
+        0xB8 => .CLV,
+        0xB9 => .LDA,
+        0xBA => .TSX,
+        0xBC => .LDY,
+        0xBD => .LDA,
+        0xBE => .LDX,
+        // 0xBF => .LAX,
+        0xC0 => .CPY,
+        0xC1 => .CMP,
+        0xC4 => .CPY,
+        0xC5 => .CMP,
+        0xC6 => .DEC,
+        0xC8 => .INY,
+        0xC9 => .CMP,
+        0xCA => .DEX,
+        // 0xCB => .AXS,
+        0xCC => .CPY,
+        0xCD => .CMP,
+        0xCE => .DEC,
+        0xD0 => .BNE,
+        0xD1 => .CMP,
+        0xD5 => .CMP,
+        0xD6 => .DEC,
+        0xD8 => .CLD,
+        0xD9 => .CMP,
+        0xDD => .CMP,
+        0xDE => .DEC,
+        0xE0 => .CPX,
+        0xE1 => .SBC,
+        0xE4 => .CPX,
+        0xE5 => .SBC,
+        0xE6 => .INC,
+        0xE8 => .INX,
+        0xE9 => .SBC,
+        0xEA => .NOP,
+        0xEC => .CPX,
+        0xED => .SBC,
+        0xEE => .INC,
+        0xF0 => .BEQ,
+        0xF1 => .SBC,
+        0xF5 => .SBC,
+        0xF6 => .INC,
+        0xF8 => .SED,
+        0xF9 => .SBC,
+        0xFD => .SBC,
+        0xFE => .INC,
+        else => DecodeError.InvalidOpcode,
+    };
+}
 
 pub fn main() !void {
     a = std.heap.c_allocator;
@@ -140,10 +322,13 @@ pub fn main() !void {
 
     var i: usize = 0x0400;
     while (i < 0x3399) : (i += 1) {
-        const opc = bin[i];
+        const opcode = bin[i];
+        const lo = bin[i + 1];
+        const hi = bin[i + 2];
 
-        std.debug.warn("{x:2}\n", .{opc});
+        std.debug.warn("{}\n", .{decode(opcode, lo, hi)});
     }
 
-    var context = Context.init();
+    const mode = AddressMode.Absolute;
+    // mode.load();
 }

5fd6c5b1f5cbeecb039622b558fab4b8e8e31327
Cameron Mann <cameron.mann@cybera.ca>
2020-09-11 09:17:12 -0600

    Report cpu clock frequency after test

--- src/cpu_test.zig
+++ src/cpu_test.zig
@@ -3,12 +3,15 @@ const assert = std.debug.assert;
 const fs = std.fs;
 const warn = std.debug.warn;
 
+const c = @cImport(@cInclude("sys/time.h"));
+
 const CPU = @import("cpu.zig");
 
 const a = std.testing.allocator;
 
 const Context = struct {
     memory: []u8,
+    cycles: u64 = 0,
 
     pub fn init() !Context {
         return Context{
@@ -17,6 +20,7 @@ const Context = struct {
     }
 
     pub fn tick(self: *Context, cur: CPU.Pins) CPU.Pins {
+        self.cycles += 1;
         var next = cur;
 
         if (cur.rw) {
@@ -46,20 +50,31 @@ test "6502_functional_test" {
     cpu.pins.a = 0x0400;
     cpu.pins.d = 0xD8;
 
+    var start = c.timeval{ .tv_sec = 0, .tv_usec = 0 };
+    var end = c.timeval{ .tv_sec = 0, .tv_usec = 0 };
+
+    _ = c.gettimeofday(&start, null);
+
     while (true) {
         const pc = cpu.registers.pc;
-        const opcode = context.memory[pc];
-        const instruction = try CPU.decode(opcode);
 
         cpu.step() catch |err| {
             if (cpu.registers.pc == 0x369B) {
                 break;
             }
 
+            const opcode = context.memory[pc];
+            const instruction = try CPU.decode(opcode);
             warn("{X}: {} {} {}\n", .{ pc, instruction.type, instruction.mode, instruction.access });
             warn("REGISTERS: {X}\n", .{cpu.registers});
             warn("P: {X}\n", .{cpu.registers.p.flags});
             return err;
         };
     }
+
+    _ = c.gettimeofday(&end, null);
+
+    const usec = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
+    const sec = @intToFloat(f64, usec);
+    warn("\nCPS: {} cycles / {d} useconds = {d} MHz\n", .{ context.cycles, sec, @intToFloat(f64, context.cycles) / sec });
 }
